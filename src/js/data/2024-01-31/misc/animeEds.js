1474133c4ac2202cf9f94801e9ca6003ba8a5486
EruisKawaii <63264196+EruisKawaii@users.noreply.github.com>
2024-06-10 00:12:37 -0700

    Update animeEds.js

--- src/js/data/2024-01-31/misc/animeEds.js
+++ src/js/data/2024-01-31/misc/animeEds.js
@@ -1,141 +1,271 @@
 export const animeEndings = [
   {
-    name: "Eikyuu Shounen: Eternal Boys - Dreamy Life by Gentlemen",
-    videoId: "https://ladist1.catbox.video/1fql1w.mp3",
+    name: "Love Live! Sunshine!! - Yume Kataru yori Yume Utaou by Nanaka Suwa, Arisa Komiya, Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/44bs2m.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - My Way by Eikyuu Shounen",
-    videoId: "https://ladist1.catbox.video/ymhvl0.mp3",
+    name: "Kaiju Girls - Joujou\u2191\u2191GAO !! by Capsule Girls",
+    videoId: "https://ladist1.catbox.video/cf2oby.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - FRIENDS by Story of Love",
-    videoId: "https://ladist1.catbox.video/d16nfu.mp3",
+    name: "Kaiju Girls - KAIJU Heart by Capsule Girls",
+    videoId: "https://ladist1.catbox.video/dciuon.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - Story of Love by Story of Love",
-    videoId: "https://ladist1.catbox.video/tq21yz.mp3",
+    name: "Love Live! Sunshine!! - Yuuki wa Doko ni? Kimi no Mune ni! by Nanaka Suwa, Arisa Komiya, Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/bitujy.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - Tsuyoshi no Maroyaka Otoko Michi by Daisuke Namikawa",
-    videoId: "https://ladist1.catbox.video/15tq3x.mp3",
+    name: "Dropkick on My Devil! - Jinbouchou Aika by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/xypp5l.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - Shall We Dance? by Gentlemen",
-    videoId: "https://ladist1.catbox.video/k26rcu.mp3",
+    name: "Love Live! Sunshine!! The School Idol Movie Over the Rainbow - Tousou Meisou M\u00f6bius Loop by Nanaka Suwa, Arisa Komiya, Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/4tqw2x.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - Eternal by Eikyuu Shounen",
-    videoId: "https://ladist1.catbox.video/o5ud40.mp3",
+    name: "Saint Seiya: Saintia Shou - Hohoemi no Resonance by Aina Suzuki & M\u30fbA\u30fbO",
+    videoId: "https://ladist1.catbox.video/3xekj0.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - GENTLEMEN by Gentlemen",
-    videoId: "https://ladist1.catbox.video/9lxd40.mp3",
+    name: "Z/X Code Reunion - Girl Meets Girl by Aina Suzuki & Miyu Tomita",
+    videoId: "https://ladist1.catbox.video/bah1sr.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - Eien ni... by Eikyuu Shounen",
-    videoId: "https://ladist1.catbox.video/kn8gyy.mp3",
+    name: "Hatena Illusion - Hikari-iro no Uta by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/nzidly.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - Again by Eikyuu Shounen",
-    videoId: "https://ladist1.catbox.video/4kmohn.mp3",
+    name: "Dropkick on My Devil!! Dash - CHI\u2606TO\u2606SE Ai Uta by Aina Suzuki & Miharu Hanai",
+    videoId: "https://ladist1.catbox.video/6glyz2.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys Next Stage - PERFECT WORLD by Story of Love",
-    videoId: "https://ladist1.catbox.video/tf4fra.mp3",
+    name: "Dropkick on My Devil!! Dash - Ave Maria by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/hzns6t.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys Next Stage - Red Line by Eikyuu Shounen",
-    videoId: "https://ladist1.catbox.video/a68drt.mp3",
+    name: "Monster Girl Doctor - Yasashisa no Namae by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/xiz7jk.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - do ones best by ???",
-    videoId: "https://files.catbox.moe/9xh5vb.mp3",
+    name: "Iwa Kakeru!: Sport Climbing Girls - Motto Takaku by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/3bmppz.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
       ]
     }
   },
   {
-    name: "Eikyuu Shounen: Eternal Boys - Step Forward by ???",
-    videoId: "https://files.catbox.moe/z65hr0.mp3",
+    name: "Iwa Kakeru!: Sport Climbing Girls - LET'S CLIMB\u2191 by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/t2o878.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
-        "OP"
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Etotama: Nyankyaku Banrai - Eto Nyanran by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/t3032r.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Etotama: Nyankyaku Banrai - blue moment by Rie Murakawa & Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/v9a7sq.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Dropkick on My Devil!!! X - Arekore Drastic by halca feat. Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/3mvu99.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Dropkick on My Devil!!! X - Thank You Dropkick! by Aina Suzuki & Hatsune Miku",
+    videoId: "https://ladist1.catbox.video/0po1d5.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Rumble Garanndoll - Reverse-Rebirth by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/vnj0zp.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Rumble Garanndoll - Akogare no Crescendo by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/9hxgqo.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Rumble Garanndoll - Pyon Pyon\u2661Love It by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/3dob74.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Rumble Garanndoll - The Burn Rumble -Burst refrain!- Yuki Aoba ver. by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/l1n8dl.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Alice Gear Aegis Expansion - Dash and Go! by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/af2akq.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "The Saint's Magic Power is Omnipotent Season 2 - Lilac Melody by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/29rijk.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Yohane The Parhelion: Sunshine in the Mirror - Hey, dear my friends by Aika Kobayashi, Rikako Aida, Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/0vpooi.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "The Weakest Tamer Began a Journey to Pick Up Trash - Hate no Nai Tabi by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/2h64st.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "Dropkick on My Devil! Apocalypse Arc - Apocalypse Day by Aina Suzuki",
+    videoId: "https://ladist1.catbox.video/nhld9j.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
       ]
     }
   }
